4fd35b55737b8b45738cdbc8e6ca35044a1a7692
Bashar Shreif <basharchreif70@gmail.com>
2025-09-22 23:27:53 +0300

    [Agent] feat(graph): Wiring aggregate subprojects node to the DAG graph

--- codewave-x-server/src/readme-agent/agent/graph.ts
+++ codewave-x-server/src/readme-agent/agent/graph.ts
@@ -14,6 +14,7 @@ import { mergeSignalsNode } from '../nodes/mergeSignals.node';
 import { writeSectionsNode } from '../nodes/writeSections.node';
 import { finalizeNode } from '../nodes/finalize.node';
 import { emitArtifactsNode } from '../nodes/emitArtifacts.node';
+import { aggregateSubprojectsNode } from '../nodes/aggregateSubproject.node';
 
 export const compileReadmeGraph = () => {
   const g = new StateGraph(Channels);
@@ -32,6 +33,7 @@ export const compileReadmeGraph = () => {
   g.addNode('WriteSections', writeSectionsNode);
   g.addNode('Finalize', finalizeNode);
   g.addNode('EmitArtifacts', emitArtifactsNode);
+  g.addNode('AggregateSubprojects', aggregateSubprojectsNode);
 
   (g as any).addEdge(START, 'IngestRepo');
   (g as any).addEdge('IngestRepo', 'Scan');
@@ -51,7 +53,8 @@ export const compileReadmeGraph = () => {
   (g as any).addEdge('Docs', 'MergeSignals');
   (g as any).addEdge('Security', 'MergeSignals');
 
-  (g as any).addEdge('MergeSignals', 'WriteSections');
+  (g as any).addEdge('MergeSignals', 'AggregateSubprojects');
+  (g as any).addEdge('AggregateSubprojects', 'WriteSections');
   (g as any).addEdge('WriteSections', 'Finalize');
   (g as any).addEdge('Finalize', 'EmitArtifacts');
   (g as any).addEdge('EmitArtifacts', END);
