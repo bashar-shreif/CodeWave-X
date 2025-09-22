d752de0a39269c5d9f3ecaf876b47c52e87ce2a0
Bashar Shreif <basharchreif70@gmail.com>
2025-09-22 20:44:32 +0300

    [Backend] feat(module): Cleaning app module

--- codewave-x-server/src/app.module.ts
+++ codewave-x-server/src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ReadmeController } from './readme/readme.controller';
 import { ReadmeModule } from './readme/readme.module';
 
 @Module({
   imports: [ReadmeModule],
-  controllers: [AppController, ReadmeController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
